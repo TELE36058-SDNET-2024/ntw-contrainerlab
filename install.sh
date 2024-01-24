6e316798152d5399a02dc287c3ca933da03b724a
Sebastian Maniak <sebastian@maniak.io>
2024-01-24 11:35:14 -0500

    adding python

--- install.sh
+++ install.sh
@@ -53,3 +53,10 @@ mv /etc/containerlab/containerlab /usr/bin && chmod a+x /usr/bin/containerlab
 
 echo "Containerlab installation is complete."
 
+
+# netmiko install 
+sudo apt-get update
+sudo apt-get install python3 python3-pip -y
+pip install netmiko
+
+sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
